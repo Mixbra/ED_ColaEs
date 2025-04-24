03a0017f9791dae06762497f221dae096006afbd
Mixbra <misael110405@gmail.com>
2025-04-24 16:27:12 -0500

    Cola

---
+++ Semana_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Semana_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ERJERCICIOS l = new ERJERCICIOS();
+           
+            l.enqueue(1);
+            l.enqueue(35);
+            l.enqueue(69);
+            l.enqueue(3432);
+            l.enqueue(23);
+            Console.WriteLine(l.ToString());
+            Console.WriteLine();
+
+            l.dequeue();
+            Console.WriteLine(l.ToString());
+            Console.WriteLine();
+
+            l.front();
+            Console.WriteLine();
+
+            Console.WriteLine(l.ToString());
+            l.size();
+            Console.WriteLine();
+
+            l.isEmpty();
+            Console.WriteLine();
+           
+
+        }
+    }
+}
